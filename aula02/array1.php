1dc554e545486d7f6367cd78b46c69430568c7fd
Beatriz Vasconcellos Espindola <165811496+beatrizve16@users.noreply.github.com>
2024-08-26 15:17:35 -0300

    Create array1.php

---
+++ aula02/array1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+$num = array(1,2,3,4,5,6,7,8,9,10);
+echo $num[0];
+echo " , ";
+echo $num[9];
+
+
+
+    ?>
+</body>
+</html>
