611684f85b9e50bf72dbe045f59940ad39db2190
Beatriz Vasconcellos Espindola <165811496+beatrizve16@users.noreply.github.com>
2024-08-26 14:34:58 -0300

    Create DoWhile1.php

---
+++ aula02/DoWhile1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+$b = 1;
+
+   do{
+
+ $b*=2;
+   } while ($contador <= 10);
+
+echo'numero final: $b';
+    ?>
+</body>
+</html>
