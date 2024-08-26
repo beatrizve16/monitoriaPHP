dfbac90335dba6441500073f437cee9e7b7a2902
Beatriz Vasconcellos Espindola <165811496+beatrizve16@users.noreply.github.com>
2024-08-26 15:40:45 -0300

    Create exercicios1.php

---
+++ aula02/exercicios1.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+$num = array(23, 67, 12, 89, 34, 56, 78, 90, 3, 45);
+
+
+for($i = 0; $i <= 10 ; $i++ ){ 
+
+
+
+}
+
+
+
+
+    ?>
+</body>
+</html>
