cd32d00c2b39c92c819fe338fcf73e411b9a41ac
Beatriz Vasconcellos Espindola <165811496+beatrizve16@users.noreply.github.com>
2024-08-29 15:39:58 -0300

    Create 3.php

---
+++ aula02/funções/3.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+function soma($numeros){
+$soma = 0;
+for($i = 0; $<6; $i++ ){
+    $soma += $numeros[i];
+}
+return $soma;
+}
+
+$numeros = [2,4,6,8,10, 12];
+
+$resposta = soma($numeros);
+
+echo "O resultado e " $soma;
+
+    ?>
+</body>
+</html>
