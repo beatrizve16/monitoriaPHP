c304cdeecb45ae5fec1518d6d790f82403720aa8
Beatriz Vasconcellos Espindola <165811496+beatrizve16@users.noreply.github.com>
2024-09-05 15:40:39 -0300

    Create questao3.php

---
+++ forms/questao3.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+$nome = $_POST["nome"];
+$ano = $_POST["ano"];
+echo "Seu nome é $nome" , "e seu ano de nascimento: $ano";
+?>
+    
+</body>
+</html>
